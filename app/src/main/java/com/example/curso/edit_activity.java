c70986317c9c7e48ff64a97252111c8b7b7da94e
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2021-07-08 22:08:39 -0400

    Uploading the repository_fernando. Worked on the Calendar Dialog in the edit activity. Worked on the edit_activity design. I have incorporated a table to dosplay the fields.

--- app/src/main/java/com/example/curso/edit_activity.java
+++ app/src/main/java/com/example/curso/edit_activity.java
@@ -2,17 +2,53 @@ package com.example.curso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class edit_activity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView mDisplayDate, mDisplayHour;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
         Intent intent = getIntent();
+
+        mDisplayDate = (TextView)findViewById(R.id.ea_input_date);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        edit_activity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + month + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
     }
     // Method to call receive the main activity
 
